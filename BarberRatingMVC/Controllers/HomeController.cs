e7022444ea3a59f82a402879f09b0176f65779fa
peshopbs2 <peshopbs2@gmail.com>
2025-03-22 02:14:14 -0800

    add dashboard

--- BarberRatingMVC/Controllers/HomeController.cs
+++ BarberRatingMVC/Controllers/HomeController.cs
@@ -1,20 +1,27 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BarberRatingMVC.Models;
+using BarberRatingMVC.Data;
 
 namespace BarberRatingMVC.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private ApplicationDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
+        ViewData["Barbers"] = _context.Barber.Count();
+        ViewData["Reviews"] = _context.Review.Count();
+        ViewData["Users"] = _context.Users.Count();
+
         return View();
     }
 
